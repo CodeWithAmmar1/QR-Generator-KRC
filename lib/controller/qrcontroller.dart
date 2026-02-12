8d0146033225911b30197ccf7adc8f96611a2622
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2026-02-12 22:28:36 +0500

    update download option

--- lib/controller/qrcontroller.dart
+++ lib/controller/qrcontroller.dart
@@ -1,16 +1,14 @@
-import 'dart:developer';
-
 import 'package:get/get.dart';
 import 'package:screenshot/screenshot.dart';
 // ignore: avoid_web_libraries_in_flutter, deprecated_member_use
-import 'dart:html' as html; 
+import 'dart:html' as html;
 
 class QRGeneratorController extends GetxController {
   var ssid = "".obs;
   var password = "".obs;
   var isHidden = false.obs;
   final ScreenshotController screenshotController = ScreenshotController();
-  
+
   String _escape(String value) {
     return value
         .replaceAll('\\', '\\\\')
@@ -21,24 +19,32 @@ class QRGeneratorController extends GetxController {
 
   String get qrData {
     if (ssid.value.isEmpty) return "";
-    String encryption = "WPA"; 
+    String encryption = "WPA";
     String escapedSsid = _escape(ssid.value);
     String escapedPass = _escape(password.value);
     String hidden = isHidden.value ? "true" : "false";
+
     return "WIFI:T:$encryption;S:$escapedSsid;P:$escapedPass;H:$hidden;;";
   }
 
-  Future<void> downloadQR() async {
-    try {
-      final imageBytes = await screenshotController.capture();
-      
-      if (imageBytes != null) {
-        final blob = html.Blob([imageBytes]);
-        final url = html.Url.createObjectUrlFromBlob(blob); 
-        html.Url.revokeObjectUrl(url);
-      }
-    } catch (e) {
-      log("Download failed: $e");
+Future<void> downloadQR() async {
+  try {
+    final imageBytes = await screenshotController.capture();
+    
+    if (imageBytes != null) {
+      final blob = html.Blob([imageBytes], 'image/png');
+      final url = html.Url.createObjectUrlFromBlob(blob);
+      final anchor = html.AnchorElement(href: url)
+        ..setAttribute("download", "${ssid.value}_wifi_qr.png")
+        ..style.display = 'none'; 
+      html.document.body?.append(anchor);
+      anchor.click();
+      anchor.remove();
+      html.Url.revokeObjectUrl(url);
     }
+  } catch (e) {
+    print("Download error: $e");
   }
+}
+  
 }
